89c90598b70b024d90e001349f31f1d28f0d5586
Enviado98 <152241479+Enviado98@users.noreply.github.com>
2025-10-11 16:45:20 -0400

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', function() {
-    console.log("Página cargada con éxito.");
-    
-    // Placeholder for future interactive features or Supabase updates
-});
